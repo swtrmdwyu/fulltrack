e6f1daba99bbaa7b77c0c37ffe107c057ae39a35
caique <caique.dimeu@gmail.com>
2024-03-01 14:59:51 -0300

    Definição de variaveis de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import styled from "styled-components"
+import './styles/_variables.scss'; 
+
+const StyledDiv = styled.div`
+  background-color: $primary-color;
+  height: 100px;
+  width: 100px;
+`
+
 export default function App() {
   return (
-    <></>
+    <StyledDiv></StyledDiv>
   )
 }
